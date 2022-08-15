b9c6f53a832d5f3ba5fab1e5727acb57e8a358c7
Juan <juanoliveira2796@gmail.com>
2022-08-15 16:14:41 -0300

    versao 3

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ function selecionarDoce(nomeClasse) {
 }
 
 function liberaBotao() {
-    const elemento = document.querySelector('.botao')
-    const paragrafo = document.querySelector('.botao p')
+    const elemento = document.querySelector('.botao');
+    const paragrafo = document.querySelector('.botao p');
     const teste = document.querySelectorAll("div.selecionado"); // pega todas as divs
     console.log(teste.length);                                  // que tem a classe .selecionado
 
@@ -49,4 +49,34 @@ function liberaBotao() {
         elemento.classList.add('botao-liberado')
         paragrafo.innerHTML = 'Fechar pedido' 
     }
+}
+
+function msgConfirmacao() {
+    // Calcular o preço final
+    const nomeComida = document.querySelector('.comidas .selecionado h2').innerHTML;
+    const nomeBebida = document.querySelector('.bebidas .selecionado h2').innerHTML;
+    const nomeSobremesa = document.querySelector('.sobremesas .selecionado h2').innerHTML;
+
+
+    const precoComida = document.querySelector('.comidas .selecionado h3').innerHTML;
+    const precoBebida = document.querySelector('.bebidas .selecionado h3').innerHTML;
+    const precoSobremesa = document.querySelector('.sobremesas .selecionado h3').innerHTML;
+    
+    const precoComidaFloat = Number(precoComida.replace("R$ ","").replace(",","."));
+    const precoBebidaFloat = Number(precoBebida.replace("R$ ","").replace(",","."));
+    const precoSobremesaFloat = Number(precoSobremesa.replace("R$ ","").replace(",","."));
+
+    console.log(precoComidaFloat);
+    console.log(precoBebidaFloat);
+    console.log(precoSobremesaFloat);
+
+    const precoFinal = precoComidaFloat+precoBebidaFloat+precoSobremesaFloat;
+    
+    // mandar msg no zap
+    const mensagem = "Olá, gostaria de fazer o pedido: \n - Prato: " + nomeComida + "\n - Bebida: " + nomeBebida + "\n - Sobremesa: " + nomeSobremesa + "\n Total: R$ " + precoFinal.toFixed(2); 
+    const msgWpp = encodeURIComponent(mensagem);
+    const link = "https://wa.me/5521994647802?text=" + msgWpp ;
+    window.open(link);
+    
+
 }
